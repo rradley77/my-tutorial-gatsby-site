f70cdfa1cf30717d9ce80feeb4da25407fa3eb66
rradley77 <rcr77@me.com>
2021-11-23 11:12:43 -0500

    updated index file to original

---
+++ src/pages/old_index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Fragment } from "react";
+import "./global.css";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <header className="main">
+        <h1>My homepage</h1>;
+      </header>
+    </Fragment>
+  );
+};
+
+export default HomePage;
