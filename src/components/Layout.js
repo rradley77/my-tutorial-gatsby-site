5bc94970daa1e0074f687fbc58f412e00b55729b
rradley77 <rcr77@me.com>
2021-11-23 13:09:48 -0500

    Gatsby tutorial part II

---
+++ src/components/Layout.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "gatsby";
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} from "./Layout.module.css";
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/">
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link lassName={navLinkText} to="/About">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/Layout";
+const About = () => {
+  return (
+    <Layout pageTitle="About My Page">
+      <p>Hi there my name is Rob!!!</p>
+    </Layout>
+  );
+};
+
+export default About;
